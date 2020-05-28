da85e278aefbce8362987a10380ee404591a9106
Margarita <kirilchuk.margarita@gmail.com>
2020-05-28 14:48:01 +0300

    junit-platform added

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -36,4 +36,34 @@ public class CashbackHackServiceTest {
 
         assertEquals(actual,expected);
     }
-}
+
+    @org.junit.jupiter.api.Test
+    public void CalculateIfAmount1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+
+        assertEquals(actual, expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void CalculateIf0(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 0;
+        int actual = service.remain(amount);
+        int expected = 1000;
+
+        assertEquals(actual,expected);
+    }
+
+    @org.junit.jupiter.api.Test
+    public void CalculateIfLessThan1000(){
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+
+        assertEquals(actual,expected);
+    }
+    }
